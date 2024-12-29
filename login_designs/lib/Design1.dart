981740d2c3a501a4dc1bf52ea3b4ffcf47fbb2f4
Abdullah Usman <abdusman660@gmail.com>
2024-12-29 17:18:08 +0500

    New Page

--- login_designs/lib/Design1.dart
+++ login_designs/lib/Design1.dart
@@ -99,9 +99,39 @@ class LoginDesign extends StatelessWidget{
                               hintStyle: TextStyle(color:Colors.grey[400],)
                             )
                           )
-                        )
+                        ),
+                        Container(  
+                          padding: EdgeInsets.all(8.0),
+                          child:TextField(
+                            decoration:InputDecoration(  
+                              border:InputBorder.none,
+                              hintText: 'Password',
+                              hintStyle: TextStyle(color:Colors.grey[400],)
+                            )
+                          )
+                        ),
                       ],)
-                  )
+                  ),
+                  SizedBox(
+                    height:30,
+                    child:Container(  
+                      height:50,
+                      decoration: BoxDecoration( 
+                        borderRadius: BorderRadius.circular(10),
+                        gradient: LinearGradient(colors:[
+                          Color.fromRGBO(143, 148, 251, 1),
+                          Color.fromRGBO(143,148, 251, .6),
+                        ]),
+                      ),
+                      child:Center(
+                        child:Text('Login',style:TextStyle(color:Colors.white,fontWeight: FontWeight.bold)),
+                      ),
+                    ),
+                  ),
+                  SizedBox(height:70,
+                  child:Center(
+                    child: Text('Forgot Password?',style:TextStyle(color:Color.fromRGBO(143, 148, 251, 1))),)),
+                  
                 ]
               )
             ),
