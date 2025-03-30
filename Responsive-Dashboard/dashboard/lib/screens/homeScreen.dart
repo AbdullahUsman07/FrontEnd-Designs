5388ef4fdffd9671fa77e1ef1fdbfe8503c73d47
Abdullah Usman <abdusman660@gmail.com>
2025-03-30 14:37:48 +0500

    feat: initial commit

---
+++ Responsive-Dashboard/dashboard/lib/screens/homeScreen.dart
@@ -0,0 +1,20 @@
+
+
+import 'package:flutter/material.dart';
+
+class Dashboard extends StatelessWidget {
+  const Dashboard({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Row(
+        children: [
+          Container(),
+          Container(),
+          Container(),
+        ],
+      ),
+    );
+  }
+}
