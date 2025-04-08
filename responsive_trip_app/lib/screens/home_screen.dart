85a9599ccf70952c8261cf77a3d52e7890d0b994
Abdullah Usman <abdusman660@gmail.com>
2025-04-08 10:36:08 +0500

    feat: new project setup completed

---
+++ responsive_trip_app/lib/screens/home_screen.dart
@@ -0,0 +1,44 @@
+
+
+import 'package:flutter/material.dart';
+import 'package:responsive_trip_app/widgets/responsive_widget.dart';
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Trip_App"),
+        centerTitle: true,
+        backgroundColor: Colors.green,
+      ),
+      body: ResponsiveWidget(
+        mobile: buildMobile(),
+         desktop: buildDesktop(),
+         tablet: buildTablet(),),
+    );
+  }
+  
+  buildMobile() {
+    return Container(
+      color: Colors.blue,
+      child: Center(child: Text('This is Mobile'),),
+    );
+  }
+
+  buildDesktop(){
+    return Container(
+      color: Colors.orange,
+      child: Center(child: Text('This is Desktop'),),
+    );
+  }
+
+  buildTablet(){
+    return Container(
+      color:Colors.pink,
+      child: Center(child: Text('This is Tablet'),),
+    );
+  }
+}
