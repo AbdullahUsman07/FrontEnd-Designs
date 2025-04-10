2f146a90a3aa3c06682b72da29358e61b7a16c0d
Abdullah Usman <abdusman660@gmail.com>
2025-04-10 11:30:25 +0500

    feat: description screen updated

--- responsive_trip_app/lib/widgets/place_description_widget.dart
+++ responsive_trip_app/lib/widgets/place_description_widget.dart
@@ -1,5 +1,3 @@
-
-
 import 'package:auto_size_text/auto_size_text.dart';
 import 'package:flutter/material.dart';
 import 'package:responsive_trip_app/model/place.dart';
@@ -10,10 +8,9 @@ class PlaceDescriptionWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-
     double fontSize = MediaQuery.of(context).size.width * 0.025;
 
-    return ListView(  
+    return ListView(
       children: [
         Image.asset(
           place.image,
@@ -22,38 +19,83 @@ class PlaceDescriptionWidget extends StatelessWidget {
           fit: BoxFit.cover,
         ),
         buildTitle(fontSize),
+        const SizedBox(height: 7),
+        buildButtons(),
+        const SizedBox(height: 10),
+        buildDescription(fontSize),
       ],
     );
   }
-  
+
   Widget buildTitle(double fontsize) {
-    return Row(  
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: Row(
+        children: [
+          Expanded(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.start,
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                AutoSizeText(
+                  place.title,
+                  minFontSize: 16,
+                  maxFontSize: 32,
+                  style: TextStyle(fontSize: fontsize),
+                ),
+                const SizedBox(height: 2),
+                AutoSizeText(
+                  place.subtitle,
+                  minFontSize: 12,
+                  maxFontSize: 22,
+                  style: TextStyle(fontSize: fontsize),
+                ),
+              ],
+            ),
+          ),
+          Icon(Icons.star, color: Colors.green[500]),
+          const SizedBox(width: 3),
+          Text('41'),
+          const SizedBox(width: 2),
+        ],
+      ),
+    );
+  }
+
+  Widget buildButtons() {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
       children: [
-        Expanded(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.start,
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              AutoSizeText(
-                place.title,
-                minFontSize: 16,
-                maxFontSize: 32,
-                style: TextStyle(fontSize: fontsize),
-              ),
-              const SizedBox(height: 2,),
-              AutoSizeText(
-                place.subtitle,
-                minFontSize: 12,
-                maxFontSize: 22,
-                style: TextStyle(fontSize: fontsize),
-              ),
-            ],
-        )),
-        Icon(Icons.star,color: Colors.red[500],),
-        const SizedBox(width: 3,),
-        Text('41'),
-        const SizedBox(width: 2,),
+        buildButton('CALL', Colors.green, Icons.call),
+        buildButton('LOCATION', Colors.green, Icons.location_on),
+        buildButton('SHARE', Colors.green, Icons.share_rounded),
       ],
+    );
+  }
+
+  Widget buildButton(String label, Color color, IconData icon) {
+    return Padding(
+      padding: EdgeInsets.all(5),
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          Icon(icon, color: color),
+          const SizedBox(height: 3),
+          Text(label, style: TextStyle(fontSize: 12, color: color)),
+        ],
+      ),
+    );
+  }
+
+  Widget buildDescription(double fontsize) {
+    return Padding(
+      padding: const EdgeInsets.all(10),
+      child: AutoSizeText(
+        place.description,
+        minFontSize: 16,
+        maxFontSize: 22,
+        style: TextStyle(fontSize: fontsize),
+      ),
     );
   }
 }
